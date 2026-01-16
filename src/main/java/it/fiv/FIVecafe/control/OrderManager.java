a7292a3b6cc73d64eb12ed61b36337f275d27074
Sara <168209418+saraiannn@users.noreply.github.com>
2026-01-16 22:35:52 +0100

    Add placeOrder method to OrderManager

--- src/main/java/it/fiv/FIVecafe/control/OrderManager.java
+++ src/main/java/it/fiv/FIVecafe/control/OrderManager.java
@@ -3,9 +3,12 @@ package it.fiv.FIVecafe.control; //this class belongs to the Control layer
 import it.fiv.FIVecafe.entity.Beverage;
 
 public class OrderManager {
+    
     public void placeOrder(Beverage beverage){
+        
         System.out.println("Order placed!");
         System.out.println("Beverage: " + beverage.getName());
         System.out.printf("Price: %.2f €%n", beverage.getBeveragePrice());
+        
     }
 }
